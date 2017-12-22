789bc742e959d598c242254e0d03e93c2feb84b8
Zetao_Yang <yangzetao1996@outlook.com>
2017-12-22 10:18:32 +0800

    fix

--- scripts/appimage_desktop_gen.sh
+++ scripts/appimage_desktop_gen.sh
@@ -4,6 +4,7 @@ cat >> package/appimage/flameshot.AppDir/flameshot.desktop << EOF
 Name=Flameshot
 Name=Take graphical screenshot
 Name[es]=Tomar captura gráfica
+GenericName=Screen capture tool
 GenericName[es]=Herramienta de captura de pantalla
 Comment=Powerfull yet simple to use screenshot software.
 Comment[es]=Potente pero simple de usar software de capturas.
@@ -11,6 +12,6 @@ Exec=flameshot
 Icon=flameshot
 Terminal=false
 Type=Application
-Categories=Graphics
+Categories=Graphics;
 StartupNotify=false
 EOF
