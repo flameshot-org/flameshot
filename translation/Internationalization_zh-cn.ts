a7c55f18a0ce644db82d69f237db8ddff5ee5e1b
copie <admin@copie.cn>
2018-01-05 20:30:34 +0800

    Add Chinese translation (#52)

---
+++ translation/Internationalization_zh-cn.ts
@@ -0,0 +1,637 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AppLauncher</name>
+    <message>
+        <source>App Launcher</source>
+        <translation>应用启动器</translation>
+    </message>
+    <message>
+        <source>Choose an app to open the capture</source>
+        <translation>选择一个应用打开此截图</translation>
+    </message>
+</context>
+<context>
+    <name>AppLauncherWidget</name>
+    <message>
+        <source>Open With</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <source>Launch in terminal</source>
+        <translation>在终端中启动</translation>
+    </message>
+    <message>
+        <source>Keep open after selection</source>
+        <translation>选择后保持此窗口打开</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <source>Unable to launch in terminal.</source>
+        <translation>无法在终端中启动。</translation>
+    </message>
+    <message>
+        <source>Unable to write in</source>
+        <translation>无法写入</translation>
+    </message>
+</context>
+<context>
+    <name>ArrowTool</name>
+    <message>
+        <source>Arrow</source>
+        <translation>箭头</translation>
+    </message>
+    <message>
+        <source>Sets the Arrow as the paint tool</source>
+        <translation>选择箭头作为油漆工具</translation>
+    </message>
+</context>
+<context>
+    <name>BlurTool</name>
+    <message>
+        <source>Blur</source>
+        <translation>模糊</translation>
+    </message>
+    <message>
+        <source>Sets the Blur as the paint tool</source>
+        <translation>选择模糊作为绘画工具</translation>
+    </message>
+</context>
+<context>
+    <name>CaptureWidget</name>
+    <message>
+        <source>Select an area with the mouse, or press Esc to exit.
+Press Enter to capture the screen.
+Press Right Click to show the color picker.
+Use the Mouse Wheel to change the thickness of your tool.</source>
+        <translation>用鼠标选择一个区域,或按Esc退出。
+按Enter键捕捉屏幕。
+按鼠标右键显示颜色选择器。
+使用鼠标滚轮来改变油漆工具的厚度。</translation>
+    </message>
+    <message>
+        <source>Unable to capture screen</source>
+        <translatorcomment>无法捕获屏幕</translatorcomment>
+        <translation>无法捕获屏幕</translation>
+    </message>
+</context>
+<context>
+    <name>CircleTool</name>
+    <message>
+        <source>Circle</source>
+        <translation>圆环</translation>
+    </message>
+    <message>
+        <source>Sets the Circle as the paint tool</source>
+        <translation>选择圆环作为绘画工具</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigWindow</name>
+    <message>
+        <source>Configuration</source>
+        <translation>配置</translation>
+    </message>
+    <message>
+        <source>Button Selection</source>
+        <translation>按钮选择</translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <translation>全选</translation>
+    </message>
+    <message>
+        <source>Interface</source>
+        <translation>界面</translation>
+    </message>
+    <message>
+        <source>Filename Editor</source>
+        <translation>文件名编辑器</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>常规</translation>
+    </message>
+</context>
+<context>
+    <name>Controller</name>
+    <message>
+        <source>&amp;Configuration</source>
+        <translation>&amp;配置</translation>
+    </message>
+    <message>
+        <source>&amp;Information</source>
+        <translation>&amp;信息</translation>
+    </message>
+    <message>
+        <source>&amp;Quit</source>
+        <translation>&amp;退出</translation>
+    </message>
+</context>
+<context>
+    <name>CopyTool</name>
+    <message>
+        <source>Copy</source>
+        <translation>复制</translation>
+    </message>
+    <message>
+        <source>Copies the selecion into the clipboard</source>
+        <translation>复制选择到剪贴板</translation>
+    </message>
+</context>
+<context>
+    <name>DBusUtils</name>
+    <message>
+        <source>Unable to connect via DBus</source>
+        <translation>无法通过Dbus进行连接</translation>
+    </message>
+</context>
+<context>
+    <name>ExitTool</name>
+    <message>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <source>Leave the capture screen</source>
+        <translation>离开屏幕捕获</translation>
+    </message>
+</context>
+<context>
+    <name>FileNameEditor</name>
+    <message>
+        <source>Edit the name of your captures:</source>
+        <translation>编辑您的截图名称:</translation>
+    </message>
+    <message>
+        <source>Edit:</source>
+        <translation>编辑器:</translation>
+    </message>
+    <message>
+        <source>Preview:</source>
+        <translation>预览:</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <source>Saves the pattern</source>
+        <translation>保存样式</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>恢复</translation>
+    </message>
+    <message>
+        <source>Restores the saved pattern</source>
+        <translation>恢复保存的样式</translation>
+    </message>
+    <message>
+        <source>Clear</source>
+        <translation>清空</translation>
+    </message>
+    <message>
+        <source>Deletes the name</source>
+        <translation>删除这个名字</translation>
+    </message>
+</context>
+<context>
+    <name>FileNameHandler</name>
+    <message>
+        <source>screenshot</source>
+        <translation>屏幕截图</translation>
+    </message>
+</context>
+<context>
+    <name>FlameshotDBusAdapter</name>
+    <message>
+        <source>Unable to capture screen</source>
+        <translation>无法捕获屏幕</translation>
+    </message>
+</context>
+<context>
+    <name>GeneneralConf</name>
+    <message>
+        <source>Show help message</source>
+        <translation>显示帮助文档</translation>
+    </message>
+    <message>
+        <source>Show the help message at the beginning in the capture mode.</source>
+        <translation>在捕获之前显示帮助信息。</translation>
+    </message>
+    <message>
+        <source>Show desktop notifications</source>
+        <translation>显示桌面通知</translation>
+    </message>
+    <message>
+        <source>Show tray icon</source>
+        <translation>显示托盘图标</translation>
+    </message>
+    <message>
+        <source>Show the systemtray icon</source>
+        <translation>显示任务栏图标</translation>
+    </message>
+    <message>
+        <source>Import</source>
+        <translation>导入</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <source>Unable to read file.</source>
+        <translation>无法读取文件.</translation>
+    </message>
+    <message>
+        <source>Unable to write file.</source>
+        <translation>无法写入文件.</translation>
+    </message>
+    <message>
+        <source>Save File</source>
+        <translation>保存到文件</translation>
+    </message>
+    <message>
+        <source>Confirm Reset</source>
+        <translation>确定重置</translation>
+    </message>
+    <message>
+        <source>Are you sure you want to reset the configuration?</source>
+        <translation>你确定你想要重置配置？</translation>
+    </message>
+    <message>
+        <source>Configuration File</source>
+        <translation>配置文件</translation>
+    </message>
+    <message>
+        <source>Export</source>
+        <translation>导出</translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <translation>重置</translation>
+    </message>
+</context>
+<context>
+    <name>ImgurUploader</name>
+    <message>
+        <source>Upload to Imgur</source>
+        <translation>上传到Imgur</translation>
+    </message>
+    <message>
+        <source>Uploading Image</source>
+        <translation>正在上传</translation>
+    </message>
+    <message>
+        <source>Copy URL</source>
+        <translation>复制链接</translation>
+    </message>
+    <message>
+        <source>Open URL</source>
+        <translation>打开链接</translation>
+    </message>
+    <message>
+        <source>Image to Clipboard.</source>
+        <translation>保存文件到剪贴板。</translation>
+    </message>
+    <message>
+        <source>Unable to open the URL.</source>
+        <translation>无法打开此链接。</translation>
+    </message>
+    <message>
+        <source>URL copied to clipboard.</source>
+        <translation>复制链接到剪贴板。</translation>
+    </message>
+    <message>
+        <source>Screenshot copied to clipboard.</source>
+        <translation>截图复制到剪贴板。</translation>
+    </message>
+</context>
+<context>
+    <name>ImgurUploaderTool</name>
+    <message>
+        <source>Image Uploader</source>
+        <translation>上传图片</translation>
+    </message>
+    <message>
+        <source>Uploads the selection to Imgur</source>
+        <translation>上传选择到 Imgur</translation>
+    </message>
+</context>
+<context>
+    <name>InfoWindow</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Right Click</source>
+        <translation>右键</translation>
+    </message>
+    <message>
+        <source>Mouse Wheel</source>
+        <translation>鼠标滑轮</translation>
+    </message>
+    <message>
+        <source>Move selection 1px</source>
+        <translation>移动选择 1 px</translation>
+    </message>
+    <message>
+        <source>Resize selection 1px</source>
+        <translation>调整选择大小 1 px</translation>
+    </message>
+    <message>
+        <source>Quit capture</source>
+        <translation>退出捕获</translation>
+    </message>
+    <message>
+        <source>Copy to clipboard</source>
+        <translation>复制到剪贴板</translation>
+    </message>
+    <message>
+        <source>Save selection as a file</source>
+        <translation>将选择保存为文件</translation>
+    </message>
+    <message>
+        <source>Undo the last modification</source>
+        <translation>撤消上次修改</translation>
+    </message>
+    <message>
+        <source>Show color picker</source>
+        <translation>显示颜色选择器</translation>
+    </message>
+    <message>
+        <source>Change the tool&apos;s thickness</source>
+        <translation>改变工具的厚度</translation>
+    </message>
+    <message>
+        <source>Available shorcuts in the screen capture mode.</source>
+        <translation>屏幕捕捉模式中的可用快捷键。</translation>
+    </message>
+    <message>
+        <source>Key</source>
+        <translation>键</translation>
+    </message>
+    <message>
+        <source>Description</source>
+        <translation>描述</translation>
+    </message>
+    <message>
+        <source>&lt;u&gt;&lt;b&gt;License&lt;/b&gt;&lt;/u&gt;</source>
+        <translation>&lt;u&gt;&lt;b&gt;License&lt;/b&gt;&lt;/u&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;u&gt;&lt;b&gt;Version&lt;/b&gt;&lt;/u&gt;</source>
+        <translation>&lt;u&gt;&lt;b&gt;Version&lt;/b&gt;&lt;/u&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;u&gt;&lt;b&gt;Shortcuts&lt;/b&gt;&lt;/u&gt;</source>
+        <translation>&lt;u&gt;&lt;b&gt;快捷键&lt;/b&gt;&lt;/u&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>LineTool</name>
+    <message>
+        <source>Line</source>
+        <translation>直线</translation>
+    </message>
+    <message>
+        <source>Sets the Line as the paint tool</source>
+        <translation>将直线线设置为绘画工具</translation>
+    </message>
+</context>
+<context>
+    <name>MarkerTool</name>
+    <message>
+        <source>Marker</source>
+        <translation>标记</translation>
+    </message>
+    <message>
+        <source>Sets the Marker as the paint tool</source>
+        <translation>将标记设置为绘画工具</translation>
+    </message>
+</context>
+<context>
+    <name>MoveTool</name>
+    <message>
+        <source>Move</source>
+        <translation>移动</translation>
+    </message>
+    <message>
+        <source>Move the selection area</source>
+        <translation>移动选择区域</translation>
+    </message>
+</context>
+<context>
+    <name>PencilTool</name>
+    <message>
+        <source>Pencil</source>
+        <translation>铅笔</translation>
+    </message>
+    <message>
+        <source>Sets the Pencil as the paint tool</source>
+        <translation>将铅笔设置为绘画工具</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Save As</source>
+        <translation>另存为</translation>
+    </message>
+    <message>
+        <source>Save Error</source>
+        <translation>保存错误</translation>
+    </message>
+    <message>
+        <source>Capture saved as </source>
+        <translation>捕获保存为 </translation>
+    </message>
+    <message>
+        <source>Error trying to save as </source>
+        <translation>尝试另存为时出错 </translation>
+    </message>
+    <message>
+        <source>Unable to connect via DBus</source>
+        <translation>无法通过DBus进行连接</translation>
+    </message>
+</context>
+<context>
+    <name>RectangleTool</name>
+    <message>
+        <source>Rectangle</source>
+        <translation>实心矩形</translation>
+    </message>
+    <message>
+        <source>Sets the Rectangle as the paint tool</source>
+        <translation>将实心矩形设置为绘画工具</translation>
+    </message>
+</context>
+<context>
+    <name>SaveTool</name>
+    <message>
+        <source>Save</source>
+        <translation>保存</translation>
+    </message>
+    <message>
+        <source>Save the capture</source>
+        <translation>保存捕获</translation>
+    </message>
+</context>
+<context>
+    <name>SelectionTool</name>
+    <message>
+        <source>Rectangular Selection</source>
+        <translation>矩形选择</translation>
+    </message>
+    <message>
+        <source>Sets the Selection as the paint tool</source>
+        <translation>将矩形选择设置为绘画工具</translation>
+    </message>
+</context>
+<context>
+    <name>SizeIndicatorTool</name>
+    <message>
+        <source>Selection Size Indicator</source>
+        <translation>选择尺寸指示</translation>
+    </message>
+    <message>
+        <source>Shows the dimensions of the selection (X Y)</source>
+        <translation>显示选择的尺寸 (X Y)</translation>
+    </message>
+</context>
+<context>
+    <name>StrftimeChooserWidget</name>
+    <message>
+        <source>Century (00-99)</source>
+        <translation>世纪 (00-99)</translation>
+    </message>
+    <message>
+        <source>Year (00-99)</source>
+        <translation>年 (00-99)</translation>
+    </message>
+    <message>
+        <source>Year (2000)</source>
+        <translation>年 (2000)</translation>
+    </message>
+    <message>
+        <source>Month Name (jan)</source>
+        <translation>月 (jul)</translation>
+    </message>
+    <message>
+        <source>Month Name (january)</source>
+        <translation>月 (juliol)</translation>
+    </message>
+    <message>
+        <source>Month (01-12)</source>
+        <translation>月 (01-12)</translation>
+    </message>
+    <message>
+        <source>Week Day (1-7)</source>
+        <translation>平常日 (1-7)</translation>
+    </message>
+    <message>
+        <source>Week (01-53)</source>
+        <translation>周 (01-53)</translation>
+    </message>
+    <message>
+        <source>Day Name (mon)</source>
+        <translation>星期 (dg)</translation>
+    </message>
+    <message>
+        <source>Day Name (monday)</source>
+        <translation>星期 (diumenge)</translation>
+    </message>
+    <message>
+        <source>Day (01-31)</source>
+        <translation>天 (01-31)</translation>
+    </message>
+    <message>
+        <source>Day of Month (1-31)</source>
+        <translation>一月中的某天 (1-31)</translation>
+    </message>
+    <message>
+        <source>Day (001-366)</source>
+        <translation>天 (001-366)</translation>
+    </message>
+    <message>
+        <source>Time (%H:%M:%S)</source>
+        <translation>时间 (%H:%M:%S)</translation>
+    </message>
+    <message>
+        <source>Time (%H:%M)</source>
+        <translation>时间 (%H:%M)</translation>
+    </message>
+    <message>
+        <source>Hour (00-23)</source>
+        <translation>小时 (00-23)</translation>
+    </message>
+    <message>
+        <source>Hour (01-12)</source>
+        <translation>小时 (01-12)</translation>
+    </message>
+    <message>
+        <source>Minute (00-59)</source>
+        <translation>分钟 (00-59)</translation>
+    </message>
+    <message>
+        <source>Second (00-59)</source>
+        <translation>秒 (00-59)</translation>
+    </message>
+    <message>
+        <source>Full Date (%m/%d/%y)</source>
+        <translation>日期 (%m/%d/%y)</translation>
+    </message>
+    <message>
+        <source>Full Date (%Y-%m-%d)</source>
+        <translation>日期 (%Y-%m-%d)</translation>
+    </message>
+</context>
+<context>
+    <name>UIcolorEditor</name>
+    <message>
+        <source>UI Color Editor</source>
+        <translation>UI颜色编辑器</translation>
+    </message>
+    <message>
+        <source>Change the color moving the selectors and see the changes in the preview buttons.</source>
+        <translation>移动颜色选择并在预览按钮查看。</translation>
+    </message>
+    <message>
+        <source>Select a Button to modify it</source>
+        <translation>选择一个按钮来修改它</translation>
+    </message>
+    <message>
+        <source>Main Color</source>
+        <translation>主色</translation>
+    </message>
+    <message>
+        <source>Click on this button to set the edition mode of the main color.</source>
+        <translation>点击按钮设置主色。</translation>
+    </message>
+    <message>
+        <source>Contrast Color</source>
+        <translation>对比色</translation>
+    </message>
+    <message>
+        <source>Click on this button to set the edition mode of the contrast color.</source>
+        <translation>点击按钮设置对比色。</translation>
+    </message>
+</context>
+<context>
+    <name>UndoTool</name>
+    <message>
+        <source>Undo</source>
+        <translation>撤消</translation>
+    </message>
+    <message>
+        <source>Undo the last modification</source>
+        <translation>撤消上次修改</translation>
+    </message>
+</context>
+</TS>
