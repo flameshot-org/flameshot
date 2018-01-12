d63efd60b6b281c9e8cb6944ca256d8a73791624
lupoDharkael <izhe@hotmail.es>
2018-01-12 18:10:02 +0100

    Add missing license information

--- src/capture/tools/penciltool.h
+++ src/capture/tools/penciltool.h
@@ -5,7 +5,7 @@
 //     Flameshot is free software: you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
 //     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) const override any later version.
+//     (at your option) any later version.
 //
 //     Flameshot is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
