54c5f97e893f94a68ec14bdf5fdeb03247b82499
lupoDharkael <izhe@hotmail.es>
2018-05-16 14:02:37 +0200

    .

--- src/widgets/capture/rectgroup.cpp
+++ src/widgets/capture/rectgroup.cpp
@@ -11,22 +11,26 @@ RectGroup::RectGroup(const QVector<QRect> &v) : m_rects(v) {
 
 void RectGroup::setRects(const QVector<QRect> &v) {
     m_rects = v;
+    // Sort by x coord from higher to lower.
     std::sort(m_rects.begin(), m_rects.end(),
               [](const QRect &r1, const QRect &r2) {
-        return r1.x() < r2.x();
+        return r1.x() > r2.x();
     });
 }
 
 QRect RectGroup::getRectContainingPoint(const QPoint &p) const {
     QRect res;
+    // Get the iterator to the QRect with x() lower than p's x().
     auto it = std::upper_bound(m_rects.begin(), m_rects.end(), QRect(p, p),
                                [](const QRect &r1, const QRect &r2) {
-        return r1.x() < r2.x();
+        return r1.x() > r2.x();
     });
 
-    for (auto i = m_rects.begin(); i != it; i++) {
-        if (i->contains(p)) {
-            res = (*i);
+    // Find the rect containing the mouse in the rects with x() lower
+    // than p's x()
+    for (; it != m_rects.end(); it++) {
+        if (it->contains(p)) {
+            res = (*it);
             break;
         }
     }
