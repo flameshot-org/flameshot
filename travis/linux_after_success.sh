8cf68421fc6c192d3a979bebaf17f5b6f548ef42
Alfredo Ramos <alfredo.ramos.sanchez@gmail.com>
2018-08-11 12:47:18 -0500

    Retry the commands if they timeout

--- travis/linux_after_success.sh
+++ travis/linux_after_success.sh
@@ -5,20 +5,20 @@ set -e
 DIST_PATH=dist
 
 if [[ "${DIST}" == "trusty" ]]; then
-	curl \
+	travis_retry curl \
 		--upload-file \
 		"${DIST_PATH}"/flameshot_${ARCH}_${VERSION}.${EXTEN} \
 		"https://transfer.sh/flameshot_${ARCH}_${VERSION}.${EXTEN}"
 else
 	case "${OS}" in
 		"ubuntu"|"debian")
-			curl \
+			travis_retry curl \
 				--upload-file \
 				"${DIST_PATH}"/flameshot_${VERSION}-${DIST}-${ARCH}_${ARCH}.${EXTEN} \
 				"https://transfer.sh/flameshot_${VERSION}-${DIST}-${ARCH}_${ARCH}.${EXTEN}"
 			;;
 		"fedora")
-			curl \
+			travis_retry curl \
 				--upload-file \
 				"${DIST_PATH}"/flameshot_${VERSION}-fedora${DIST}-${ARCH}_${ARCH}.${EXTEN} \
 				"https://transfer.sh/flameshot_${VERSION}-fedora${DIST}-${ARCH}_${ARCH}.${EXTEN}"
