52d98154507fb7db9ea0928778d791ca24491959
Ahmed Zetao Yang <zetao.yang@hotmail.com>
2018-08-18 23:04:40 +0800

    Some Fixes for updating to v0.6.0 (#317)
    * Fix name specification of packages
    
    * Update rpm .spec file
    
    * Update to v0.6.0

--- travis/linux_after_success.sh
+++ travis/linux_after_success.sh
@@ -7,21 +7,21 @@ DIST_PATH=dist
 if [[ "${DIST}" == "trusty" ]]; then
 	travis_retry curl \
 		--upload-file \
-		"${DIST_PATH}"/flameshot_${ARCH}_${VERSION}.${EXTEN} \
-		"https://transfer.sh/flameshot_${ARCH}_${VERSION}.${EXTEN}"
+		"${DIST_PATH}"/flameshot_${VERSION}_${ARCH}.${EXTEN} \
+		"https://transfer.sh/flameshot_${VERSION}_${ARCH}.${EXTEN}"
 else
 	case "${OS}" in
 		"ubuntu"|"debian")
 			travis_retry curl \
 				--upload-file \
-				"${DIST_PATH}"/flameshot_${VERSION}-${DIST}-${ARCH}_${ARCH}.${EXTEN} \
-				"https://transfer.sh/flameshot_${VERSION}-${DIST}-${ARCH}_${ARCH}.${EXTEN}"
+				"${DIST_PATH}"/flameshot_${VERSION}_${DIST}_${ARCH}.${EXTEN} \
+				"https://transfer.sh/flameshot_${VERSION}_${DIST}_${ARCH}.${EXTEN}"
 			;;
 		"fedora")
 			travis_retry curl \
 				--upload-file \
-				"${DIST_PATH}"/flameshot_${VERSION}-fedora${DIST}-${ARCH}_${ARCH}.${EXTEN} \
-				"https://transfer.sh/flameshot_${VERSION}-fedora${DIST}-${ARCH}_${ARCH}.${EXTEN}"
+				"${DIST_PATH}"/flameshot_${VERSION}_fedora${DIST}_${ARCH}.${EXTEN} \
+				"https://transfer.sh/flameshot_${VERSION}_fedora${DIST}_${ARCH}.${EXTEN}"
 			;;
 	esac
 fi
