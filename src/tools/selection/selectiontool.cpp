488b5cff3924b2aa99d34a32a28088f5f18c30b9
Eric Blanquer <eblanquer@centile.com>
2020-06-24 00:27:57 +0200

    draw rounded selection using thickness radius instead of drawing simple selection

--- src/tools/selection/selectiontool.cpp
+++ src/tools/selection/selectiontool.cpp
@@ -54,8 +54,13 @@ void SelectionTool::process(QPainter &painter, const QPixmap &pixmap, bool recor
     if (recordUndo) {
         updateBackup(pixmap);
     }
-    painter.setPen(QPen(m_color, m_thickness));
-    painter.drawRect(QRect(m_points.first, m_points.second));
+    painter.setPen(QPen(m_color, PADDING_VALUE + m_thickness));
+    if (m_thickness == 0) {
+       painter.drawRect(QRect(m_points.first, m_points.second));
+    }
+    else {
+       painter.drawRoundedRect(QRect(m_points.first, m_points.second), m_thickness, m_thickness);
+    }
 }
 
 void SelectionTool::paintMousePreview(QPainter &painter, const CaptureContext &context) {
@@ -65,7 +70,7 @@ void SelectionTool::paintMousePreview(QPainter &painter, const CaptureContext &c
 
 void SelectionTool::drawStart(const CaptureContext &context) {
     m_color = context.color;
-    m_thickness = context.thickness + PADDING_VALUE;
+    m_thickness = context.thickness;
     m_points.first = context.mousePos;
     m_points.second = context.mousePos;
 }
