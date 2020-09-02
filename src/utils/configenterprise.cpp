d86bf1aea6e3a4eeaa4fec20414dd797e8677c3c
Yurii Puchkov <panpuchkov@gmail.com>
2020-09-02 07:17:59 -0700

    Fix - Calculate workdir on Windows if startup option was set before the latest fix

--- src/utils/configenterprise.cpp
+++ src/utils/configenterprise.cpp
@@ -2,6 +2,8 @@
 #include <QDir>
 #include <QSettings>
 #include <QFileInfo>
+#include <QString>
+#include <QSettings>
 
 
 ConfigEnterprise::ConfigEnterprise()
@@ -9,11 +11,18 @@ ConfigEnterprise::ConfigEnterprise()
     // get enterprise settings
     m_settings = nullptr;
     QString configIniPath = QDir(QDir::currentPath()).filePath("config.ini");
-#if defined(Q_OS_LINUX) || defined(Q_OS_UNIX)
     if(!(QFileInfo::exists(configIniPath) && QFileInfo(configIniPath).isFile())) {
+#if defined(Q_OS_LINUX) || defined(Q_OS_UNIX)
         configIniPath = "/etc/flameshot/config.ini";
-    }
+#elif defined(Q_OS_WIN)
+        // calculate workdir for flameshot on startup if is not set yet
+        QSettings bootUpSettings(
+                    "HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run",
+                    QSettings::NativeFormat);
+        QFileInfo fi(bootUpSettings.value("Flameshot").toString());
+        configIniPath = QDir(fi.absolutePath()).filePath("config.ini");
 #endif
+    }
     m_settings = new QSettings(configIniPath, QSettings::IniFormat);
 }
 
