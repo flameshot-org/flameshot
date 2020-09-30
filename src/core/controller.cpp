43f293dcb41d43e63b0523e2e5301bee5c029358
Ahmed Zetao Yang <zetao.yang@hotmail.com>
2020-09-30 22:47:12 +0800

    chore: use flameshot icon as tray notification icon
    * this need Qt >= 5.9, see https://doc.qt.io/qt-5.9/qsystemtrayicon.html#showMessage-1

--- src/core/controller.cpp
+++ src/core/controller.cpp
@@ -253,7 +253,7 @@ void Controller::sendTrayNotification(const QString& text,
 {
     if (m_trayIcon) {
         m_trayIcon->showMessage(
-          title, text, QSystemTrayIcon::Information, timeout);
+          title, text, QIcon(":img/app/flameshot.svg"), timeout);
     }
 }
 
