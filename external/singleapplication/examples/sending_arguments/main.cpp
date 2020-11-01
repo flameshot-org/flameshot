8211b6aff08191ad6c83115754c1612dbc123bdd
Jeremy Borgman <borgman.jeremy@pm.me>
2020-10-31 19:46:18 -0500

    updated qtsingleapplication to v3.1.5

---
+++ external/singleapplication/examples/sending_arguments/main.cpp
@@ -0,0 +1,26 @@
+#include "messagereceiver.h"
+#include <singleapplication.h>
+
+int main(int argc, char* argv[])
+{
+    // Allow secondary instances
+    SingleApplication app(argc, argv, true);
+
+    MessageReceiver msgReceiver;
+
+    // If this is a secondary instance
+    if (app.isSecondary()) {
+        app.sendMessage(app.arguments().join(' ').toUtf8());
+        qDebug() << "App already running.";
+        qDebug() << "Primary instance PID: " << app.primaryPid();
+        qDebug() << "Primary instance user: " << app.primaryUser();
+        return 0;
+    } else {
+        QObject::connect(&app,
+                         &SingleApplication::receivedMessage,
+                         &msgReceiver,
+                         &MessageReceiver::receivedMessage);
+    }
+
+    return app.exec();
+}
