f1004fbb8748274f20e0da81f626d33c3afd69c7
Boyuan Yang <byang@debian.org>
2021-02-13 15:29:05 -0500

    ko_KR translation: Fix locale string
    The Korean translation was using `kr_KR` locale string, which
    is a typo and will not be recognized by Qt.
    
    This commit replaces `kr_KR` with `ko_KR`.

--- data/translations/Internationalization_ko.ts
+++ data/translations/Internationalization_ko.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="kr_KR">
+<TS version="2.1" language="ko_KR">
 <context>
     <name>AppLauncher</name>
     <message>
