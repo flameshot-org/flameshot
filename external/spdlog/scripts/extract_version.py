310918d5d03142615b52c965a1b29be060576b35
SUPERustam <64551202+SUPERustam@users.noreply.github.com>
2021-04-07 14:23:35 +0300

    minor update
    f-string instead of formta method, PEP8 format

--- external/spdlog/scripts/extract_version.py
+++ external/spdlog/scripts/extract_version.py
@@ -4,14 +4,14 @@ import os
 import re
 
 base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-config_h  = os.path.join(base_path, 'include', 'spdlog', 'version.h')
-data      = {'MAJOR': 0, 'MINOR': 0, 'PATCH': 0}
-reg       = re.compile(r'^\s*#define\s+SPDLOG_VER_([A-Z]+)\s+([0-9]+).*$')
+config_h = os.path.join(base_path, 'include', 'spdlog', 'version.h')
+data = {'MAJOR': 0, 'MINOR': 0, 'PATCH': 0}
+reg = re.compile(r'^\s*#define\s+SPDLOG_VER_([A-Z]+)\s+([0-9]+).*$')
 
 with open(config_h, 'r') as fp:
-  for l in fp:
-    m = reg.match(l)
-    if m:
-      data[m.group(1)] = int(m.group(2))
+    for l in fp:
+        m = reg.match(l)
+        if m:
+            data[m.group(1)] = int(m.group(2))
 
-print('{}.{}.{}'.format(data['MAJOR'], data['MINOR'], data['PATCH']))
+print(f"{data['MAJOR']}.{data['MINOR']}.{data['PATCH']}")
