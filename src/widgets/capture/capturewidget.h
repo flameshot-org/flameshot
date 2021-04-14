e49b0e3924f8be5f7476efbf19ab67cfbe5c13eb
Yuriy Puchkov <yuriy.puchkov@namecheap.com>
2021-04-14 16:38:07 +0300

    Code refactoring - CaptureWidget::mousePressEvent function is split into a few, code became more readable
    (cherry picked from commit dc850b3daa00fa173126d86403c35f3c67c448e6)

--- src/widgets/capture/capturewidget.h
+++ src/widgets/capture/capturewidget.h
@@ -106,6 +106,9 @@ protected:
     void moveEvent(QMoveEvent* moveEvent) override;
 
 private:
+    void selectToolItemAtPos(const QPoint& pos);
+    void showColorPicker(const QPoint& pos);
+    bool startDrawObjectTool(const QPoint& pos);
     QPointer<CaptureTool> activeToolObject();
     void initContext(const QString& savePath, bool fullscreen);
     void initPanel();
