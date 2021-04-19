eebe36c48c5cb5641616678152bf6213050b4b6b
Yuriy Puchkov <yuriy.puchkov@namecheap.com>
2021-04-19 20:17:23 +0300

    Update copyright info

--- src/widgets/capture/capturetoolobjects.h
+++ src/widgets/capture/capturetoolobjects.h
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// SPDX-FileCopyrightText: 2017-2019 Alejandro Sirgo Rica & Contributors
+// SPDX-FileCopyrightText: 2021 Yurii Puchkov & Contributors
 
 #ifndef FLAMESHOT_CAPTURETOOLOBJECTS_H
 #define FLAMESHOT_CAPTURETOOLOBJECTS_H
