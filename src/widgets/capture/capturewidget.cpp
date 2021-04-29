32ed51be13e016eae26047ed750780833cfa4970
Yuriy Puchkov <yuriy.puchkov@namecheap.com>
2021-04-29 15:55:02 +0300

    fix - Text modification cannot be undone fix - Object that was created after the text object disappears after undo editing text
    (cherry picked from commit 79b2fb92aca65b177cffb386db141d30551a2b1c)

--- src/widgets/capture/capturewidget.cpp
+++ src/widgets/capture/capturewidget.cpp
@@ -310,6 +310,9 @@ void CaptureWidget::releaseActiveTool()
     if (m_activeTool) {
         if (m_activeTool->editMode()) {
             m_activeTool->setEditMode(false);
+            if (m_activeTool->isChanged()) {
+                pushObjectsStateToUndoStack();
+            }
         }
         if (-1 == m_panel->activeLayerIndex() && m_activeButton) {
             // delete tool if no active selection, otherwise object shouldn't be
@@ -1408,8 +1411,10 @@ void CaptureWidget::updateCursor()
 void CaptureWidget::pushToolToStack()
 {
     // append current tool to the new state
+    bool isChanged = true;
     if (m_activeTool && m_activeTool->editMode()) {
         m_activeTool->setEditMode(false);
+        isChanged = m_activeTool->isChanged();
     }
     if (m_activeTool && m_activeButton) {
         disconnect(this,
@@ -1428,7 +1433,9 @@ void CaptureWidget::pushToolToStack()
         releaseActiveTool();
     }
 
-    pushObjectsStateToUndoStack();
+    if (isChanged) {
+        pushObjectsStateToUndoStack();
+    }
 }
 
 void CaptureWidget::drawToolsData(bool updateLayersPanel, bool drawSelection)
