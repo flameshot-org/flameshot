3aba282c836bcc9ea1a47d7166114fc2c94bfeae
Yuriy Puchkov <yuriy.puchkov@namecheap.com>
2021-05-20 17:35:08 +0300

    fix - Upload doesn't called (active tool is not set on processTool)
    (cherry picked from commit d914b2ba7ba4d1741f6e990587d13a5d08b0c591)

--- src/widgets/capture/capturewidget.cpp
+++ src/widgets/capture/capturewidget.cpp
@@ -1069,7 +1069,8 @@ void CaptureWidget::loadDrawThickness()
 
 void CaptureWidget::processTool(CaptureTool* t)
 {
-    t->pressed(m_context);
+    m_activeTool = t;
+    m_activeTool->pressed(m_context);
 }
 
 void CaptureWidget::handleButtonSignal(CaptureTool::Request r)
