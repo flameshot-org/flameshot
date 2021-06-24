a640a58b8e1d2c6253f24a14c36e36ebaa110c20
Yaman Qalieh <ybq987@gmail.com>
2021-06-23 22:01:52 -0400

    Allow saving to any supported image format

--- src/utils/screenshotsaver.h
+++ src/utils/screenshotsaver.h
@@ -25,12 +25,5 @@ private:
     unsigned m_id;
     QString ShowSaveFileDialog(QWidget* parent,
                                const QString& title,
-                               const QString& directory,
-                               const QString& filter);
-
-    const QString pngFilter = "Portable Network Graphic file (PNG) (*.png)";
-    const QString bmpFilter = "BMP file (*.bmp)";
-    const QString jpgFilter = "JPEG file (*.jpg)";
-    const QString defaultFilter = "By extension [default: *.png] (*.png)";
-    const QString separator = ";;";
+                               const QString& directory);
 };
