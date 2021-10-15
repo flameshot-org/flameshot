3ed1ad72eea487bcba83061153613f3af0590a4b
Haris Gušić <harisgusic.dev@gmail.com>
2021-10-15 03:26:18 +0200

    Fix file extension config option
    Signed-off-by: Haris Gušić <harisgusic.dev@gmail.com>

--- src/utils/confighandler.cpp
+++ src/utils/confighandler.cpp
@@ -110,7 +110,7 @@ static QMap<class QString, QSharedPointer<ValueHandler>>
     OPTION("ignoreUpdateToVersion"       ,String             ( ""            )),
     OPTION("keepOpenAppLauncher"         ,Bool               ( false         )),
     OPTION("fontFamily"                  ,String             ( ""            )),
-    OPTION("setSaveAsFileExtension"      ,String             ( ""            )),
+    OPTION("setSaveAsFileExtension"      ,String             ( ".png"        )),
   };
 
 static QMap<QString, QSharedPointer<KeySequence>> recognizedShortcuts = {
