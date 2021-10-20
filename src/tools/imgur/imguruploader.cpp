94ed574f357787706d7fb1061f06bdf460d431fb
Mehrad Mahmoudian <m.mahmoudian@gmail.com>
2021-10-20 13:50:07 +0300

    Minor fix and modification on selectable tests in dialogs (#1990)
    * makes text of the imgur upload selectable
    
    * fix the mouse to I shape for selectable texts

--- src/tools/imgur/imguruploader.cpp
+++ src/tools/imgur/imguruploader.cpp
@@ -51,6 +51,8 @@ ImgurUploader::ImgurUploader(const QPixmap& capture, QWidget* parent)
     m_spinner->start();
 
     m_infoLabel = new QLabel(tr("Uploading Image"));
+    m_infoLabel->setTextInteractionFlags(Qt::TextSelectableByMouse);
+    m_infoLabel->setCursor(QCursor(Qt::IBeamCursor));
 
     m_vLayout = new QVBoxLayout();
     setLayout(m_vLayout);
