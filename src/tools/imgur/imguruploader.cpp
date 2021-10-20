32bbecbee13a0c617451650edc19b99314686d98
Mehrad Mahmoudian <m.mahmoudian@gmail.com>
2021-10-20 12:25:59 +0300

    fix the mouse to I shape for selectable texts

--- src/tools/imgur/imguruploader.cpp
+++ src/tools/imgur/imguruploader.cpp
@@ -52,6 +52,7 @@ ImgurUploader::ImgurUploader(const QPixmap& capture, QWidget* parent)
 
     m_infoLabel = new QLabel(tr("Uploading Image"));
     m_infoLabel->setTextInteractionFlags(Qt::TextSelectableByMouse);
+    m_infoLabel->setCursor(QCursor(Qt::IBeamCursor));
 
     m_vLayout = new QVBoxLayout();
     setLayout(m_vLayout);
