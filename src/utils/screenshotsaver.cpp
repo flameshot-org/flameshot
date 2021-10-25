329bc5d0581cfcc8b500b59e57119d2cafcde921
Lyqst <benja.appel@gmail.com>
2021-10-25 13:00:20 -0300

    Fix format

--- src/utils/screenshotsaver.cpp
+++ src/utils/screenshotsaver.cpp
@@ -88,7 +88,8 @@ bool ScreenshotSaver::saveToFilesystem(const QPixmap& capture,
                                        const QString& path,
                                        const QString& messagePrefix)
 {
-    QString completePath = FileNameHandler().properScreenshotPath(path, ConfigHandler().setSaveAsFileExtension());
+    QString completePath = FileNameHandler().properScreenshotPath(
+      path, ConfigHandler().setSaveAsFileExtension());
     bool ok = capture.save(completePath);
     QString saveMessage = messagePrefix;
     QString notificationPath = completePath;
@@ -148,7 +149,8 @@ bool ScreenshotSaver::saveToFilesystemGUI(const QPixmap& capture)
         defaultSavePath =
           QStandardPaths::writableLocation(QStandardPaths::PicturesLocation);
     }
-    QString savePath = FileNameHandler().properScreenshotPath(defaultSavePath, ConfigHandler().setSaveAsFileExtension());
+    QString savePath = FileNameHandler().properScreenshotPath(
+      defaultSavePath, ConfigHandler().setSaveAsFileExtension());
 #if defined(Q_OS_MACOS)
     for (QWidget* widget : qApp->topLevelWidgets()) {
         QString className(widget->metaObject()->className());
