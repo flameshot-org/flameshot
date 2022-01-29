3423647978fcd23b368a040bf5dcf3a30fab7e84
borgmanJeremy <46930769+borgmanJeremy@users.noreply.github.com>
2022-01-29 12:06:34 -0600

    Rewrote launcher to use a qt designer file. Removed some code related to (#2338)
    dragging the launcher corner as it didnt seem to do anything. Cleaned up
    clang format suggestions.

--- src/widgets/capturelauncher.h
+++ src/widgets/capturelauncher.h
@@ -5,35 +5,27 @@
 
 #include <QDialog>
 
-class QCheckBox;
-class QPushButton;
-class QVBoxLayout;
-class QComboBox;
-class QSpinBox;
-class QLabel;
-class ImageLabel;
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class CaptureLauncher;
+}
+QT_END_NAMESPACE
 
 class CaptureLauncher : public QDialog
 {
     Q_OBJECT
+
 public:
     explicit CaptureLauncher(QDialog* parent = nullptr);
+    ~CaptureLauncher();
 
 private:
-    void connectCaptureSlots();
-    void disconnectCaptureSlots();
+    Ui::CaptureLauncher* ui;
+    void connectCaptureSlots() const;
+    void disconnectCaptureSlots() const;
 
 private slots:
     void startCapture();
-    void startDrag();
-    void captureTaken(QPixmap p, const QRect& selection);
+    void captureTaken(QPixmap p);
     void captureFailed();
-
-private:
-    QSpinBox* m_delaySpinBox;
-    QComboBox* m_captureType;
-    QVBoxLayout* m_mainLayout;
-    QPushButton* m_launchButton;
-    QLabel* m_CaptureModeLabel;
-    ImageLabel* m_imageLabel;
 };
