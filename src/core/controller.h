9498e23d05e1e6f5e2de00b037da0cf78172df4b
borgmanJeremy <46930769+borgmanJeremy@users.noreply.github.com>
2022-01-31 15:21:00 -0600

    Upload history UI (#2345)
    * Refactored upload history widget to use qt ui files. Cleaned up clang
    tidy warnings. Remove copied to clipboard notified.
    
    * fixed some irregularities with the layout
    
    * fixing forward declaration
    
    (cherry picked from commit 42917930ca11646ad5ac71f53f600e752ed713c6)

--- src/core/controller.h
+++ src/core/controller.h
@@ -17,7 +17,7 @@ class ConfigWindow;
 class InfoWindow;
 class QSystemTrayIcon;
 class CaptureLauncher;
-class HistoryWidget;
+class UploadHistory;
 class QNetworkAccessManager;
 class QNetworkReply;
 #if (defined(Q_OS_MAC) || defined(Q_OS_MAC64) || defined(Q_OS_MACOS) ||        \
