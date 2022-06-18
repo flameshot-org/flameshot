a44018fcc390e5412f54326b836071828f24dda0
Alaskra <958328065@qq.com>
2022-06-18 22:18:59 +0800

    Another way to fix Launchapp unexpectly close (#2675)
    * fix unexpected close when launch external app
    
    * update
    
    * another way to fix launchapp crash
    
    * format

--- src/core/flameshot.cpp
+++ src/core/flameshot.cpp
@@ -37,6 +37,7 @@
 
 Flameshot::Flameshot()
   : m_captureWindow(nullptr)
+  , m_have_external_widgets(false)
 #if defined(Q_OS_MACOS)
   , m_HotkeyScreenshotCapture(nullptr)
   , m_HotkeyScreenshotHistory(nullptr)
@@ -411,12 +412,13 @@ void Flameshot::exportCapture(QPixmap capture,
     if (!(tasks & CR::UPLOAD)) {
         emit captureTaken(capture);
     }
-    // hacks: close a window to trigger qt's quitOnLastWindowClose
-    // if not create tmp_window and close, the `flameshot gui` won't exit after
-    // click copy button
-    QWidget* tmp = new QWidget();
-    tmp->show();
-    tmp->close();
+    if (!m_have_external_widgets)
+        qApp->quit();
+}
+
+void Flameshot::setExternalWidgets()
+{
+    m_have_external_widgets = true;
 }
 
 // STATIC ATTRIBUTES
