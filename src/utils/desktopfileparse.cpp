6510729b46503c7d09d8ae53e2387b203504f021
Al Williams <al.williams@awce.com>
2022-06-30 13:20:19 -0500

    DesktopFileParser only reads .desktop files

--- src/utils/desktopfileparse.cpp
+++ src/utils/desktopfileparse.cpp
@@ -100,7 +100,11 @@ DesktopAppData DesktopFileParser::parseDesktopFile(const QString& fileName,
 
 int DesktopFileParser::processDirectory(const QDir& dir)
 {
-    QStringList entries = dir.entryList(QDir::NoDotAndDotDot | QDir::Files);
+    // Note that https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+    // says files must end in .desktop or .directory
+    // So filtering by .desktop stops us reading things like editor backups
+    // .kdelnk is long deprecated
+    QStringList entries = dir.entryList({"*.desktop"},QDir::NoDotAndDotDot | QDir::Files);
     bool ok;
     int length = m_appList.length();
     for (QString file : entries) {
