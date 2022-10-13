37266ac0a1d133415f3b69c31d5888a36eec9aea
borgmanJeremy <46930769+borgmanJeremy@users.noreply.github.com>
2022-10-13 08:19:34 -0500

    Adding pin transparency (#2944)
    * Adding pin transparency
    
    * added hotkeys

--- src/tools/pin/pinwidget.h
+++ src/tools/pin/pinwidget.h
@@ -23,6 +23,7 @@ protected:
     void mouseDoubleClickEvent(QMouseEvent*) override;
     void mousePressEvent(QMouseEvent*) override;
     void mouseMoveEvent(QMouseEvent*) override;
+    void keyPressEvent(QKeyEvent*) override;
     void enterEvent(QEvent*) override;
     void leaveEvent(QEvent*) override;
 
@@ -38,6 +39,9 @@ private:
     void rotateLeft();
     void rotateRight();
 
+    void increaseOpacity();
+    void decreaseOpacity();
+
     QPixmap m_pixmap;
     QVBoxLayout* m_layout;
     QLabel* m_label;
@@ -48,6 +52,7 @@ private:
 
     bool m_expanding{ false };
     qreal m_scaleFactor{ 1 };
+    qreal m_opacity{ 1 };
     unsigned int m_rotateFactor{ 0 };
     qreal m_currentStepScaleFactor{ 1 };
     bool m_sizeChanged{ false };
