015294609951819b2becf2fc0d2d51e18d93ed9d
Jeremy Borgman <borgman.jeremy@pm.me>
2023-03-06 11:46:14 -0600

    adding a flake build

---
+++ default.nix
@@ -0,0 +1,37 @@
+{ stdenv
+, lib
+, libsForQt5
+, wrapQtAppsHook
+, cmake
+, qtbase
+, qttools
+, qtsvg 
+}:
+
+with libsForQt5;
+
+stdenv.mkDerivation rec {
+  pname = "flameshot";
+  version = "0.1.0";
+  
+  src = ./.;
+
+  nativeBuildInputs = [ 
+    cmake 
+    qttools
+    qtsvg
+    wrapQtAppsHook
+    ];
+  buildInputs = [ qtbase ];
+
+  cmakeFlags = [
+  ];
+
+  meta = with lib; {
+    homepage = "";
+    description = ''
+    '';
+    licencse = licenses.gpl3Plus;
+    platforms = with platforms; linux ++ darwin;
+  };
+}
