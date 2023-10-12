6e9ccb52a0c8da0c540c77757986eb7e2ed3b9a9
Jeremy Borgman <borgman.jeremy@pm.me>
2023-10-12 08:09:00 -0500

    bumped version

--- default.nix
+++ default.nix
@@ -1,37 +1,36 @@
-{ stdenv
-, lib
-, libsForQt5
-, wrapQtAppsHook
-, cmake
-, qtbase
-, qttools
-, qtsvg 
+{
+  stdenv,
+  lib,
+  libsForQt5,
+  wrapQtAppsHook,
+  cmake,
+  qtbase,
+  qttools,
+  qtsvg,
 }:
-
 with libsForQt5;
+  stdenv.mkDerivation rec {
+    pname = "flameshot";
+    version = "0.3.0";
 
-stdenv.mkDerivation rec {
-  pname = "flameshot";
-  version = "0.2.0";
-  
-  src = ./.;
+    src = ./.;
 
-  nativeBuildInputs = [ 
-    cmake 
-    qttools
-    qtsvg
-    wrapQtAppsHook
+    nativeBuildInputs = [
+      cmake
+      qttools
+      qtsvg
+      wrapQtAppsHook
     ];
-  buildInputs = [ qtbase ];
+    buildInputs = [qtbase];
 
-  cmakeFlags = [
-  ];
+    cmakeFlags = [
+    ];
 
-  meta = with lib; {
-    homepage = "";
-    description = ''
-    '';
-    licencse = licenses.gpl3Plus;
-    platforms = with platforms; linux ++ darwin;
-  };
-}
+    meta = with lib; {
+      homepage = "";
+      description = ''
+      '';
+      licencse = licenses.gpl3Plus;
+      platforms = with platforms; linux ++ darwin;
+    };
+  }
