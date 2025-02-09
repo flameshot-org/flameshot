c35e16afcb4c8175465bd165c2f65020656c852b
Matthias Kunnen <matthias.kunnen@gmail.com>
2025-02-09 11:42:58 +0100

    Fix English translations not used despite being preferred (#3822)
    When specifying the preferred language on Linux, the $LANGUAGE env var
    is used. https://www.gnu.org/software/gettext/manual/html_node/The-LANGUAGE-variable.html
    
    Example:
    $ LANGUAGE=en:nl flameshot
    
    Expected:
    flameshot should be in English as this is a localization that flameshot provides.
    If English would not be supported, it should fall back to using Dutch (nl).
    
    Actual:
    flameshot uses Dutch translations.
    
    Cause:
    If an empty translation file for "en" is not provided, English is only
    used as a fallback.
    See https://bugreports.qt.io/browse/QTBUG-69196

---
+++ data/translations/Internationalization_en.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<!--
+    An empty translation file is required for the base language, or it will only be used as a
+    fallback if the user prefers other supported languages. E.g.
+    $ LANGUAGE=en:nl flameshot
+    will result in Dutch translations being used despite English being higher priority.
+    See https://bugreports.qt.io/browse/QTBUG-69196
+-->
+<TS version="2.1" language="en">
+</TS>
