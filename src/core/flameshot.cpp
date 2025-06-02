13f08f9ba0b3b115e4ef62eced6836e6f46ebbc5
El Thoro <ElTh0r0@users.noreply.github.com>
2025-06-02 13:32:34 +0200

    Forgot another deprecated QApplication::desktop (macOS) / fix Qt 6 Windows build (#3967)
    * Forgot another deprecated QApplication::desktop (macOS)
    
    * Fix Qt 6 Windows build

--- src/core/flameshot.cpp
+++ src/core/flameshot.cpp
@@ -52,8 +52,7 @@ Flameshot::Flameshot()
     // permissions on the first run. Otherwise it will be hidden under the
     // CaptureWidget
     QScreen* currentScreen = QGuiAppCurrentScreen().currentScreen();
-    // TODO: Qt 6 - QApplication::desktop() not avialable
-    currentScreen->grabWindow(QApplication::desktop()->winId(), 0, 0, 1, 1);
+    currentScreen->grabWindow(0, 0, 0, 1, 1);
 
     // set global shortcuts for MacOS
     m_HotkeyScreenshotCapture = new QHotkey(
