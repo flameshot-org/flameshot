932bf7fb20b46dc1f96e0b3bedcaeb577719add8
ionsquare <ionsquare@users.noreply.github.com>
2025-08-07 08:56:14 -0700

    Fix clang-format issue

--- src/widgets/capture/capturewidget.cpp
+++ src/widgets/capture/capturewidget.cpp
@@ -293,7 +293,7 @@ CaptureWidget::~CaptureWidget()
         Flameshot::instance()->exportCapture(
           pixmap(), geometry, m_context.request);
     } else {
-        emit Flameshot::instance() -> captureFailed();
+        emit Flameshot::instance()->captureFailed();
     }
 }
 
