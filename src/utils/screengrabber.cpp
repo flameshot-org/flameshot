3ef8f093d35518cc22880154d0c0e774e6403b24
borgmanJeremy <46930769+borgmanJeremy@users.noreply.github.com>
2026-02-19 06:03:53 -0600

    fix build issue in debug mode and cleanup wording (#4525)
    * fix build issue in debug mode and cleanup wording
    
    * clang format

--- src/utils/screengrabber.cpp
+++ src/utils/screengrabber.cpp
@@ -504,13 +504,12 @@ QPixmap ScreenGrabber::cropToMonitor(const QPixmap& fullScreenshot,
                                  targetPhysicalHeight,
                                  Qt::IgnoreAspectRatio,
                                  Qt::SmoothTransformation);
-    }
 #ifdef FLAMESHOT_DEBUG_CAPTURE
-    qDebug() << tr("Scaling screenshot to: %1 %2")
-                  .arg(targetPhysicalWidth)
-                  .arg(targetPhysicalHeight);
+        qDebug() << tr("Scaling screenshot to: %1 %2")
+                      .arg(targetPhysicalWidth)
+                      .arg(targetPhysicalHeight);
 #endif
-
+    }
 #endif
     // Cropped region should be at target monitor's native DPR
     cropped.setDevicePixelRatio(targetDpr);
