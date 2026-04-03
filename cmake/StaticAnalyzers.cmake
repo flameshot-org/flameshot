6a9609f02f05e919344375abf494a894bff19965
ElTh0r0 <elthoro@gmx.de>
2026-04-03 17:40:56 +0200

    Add Qt deprecation check

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,6 +1,7 @@
 option(ENABLE_CPPCHECK "Enable static analysis with cppcheck" OFF)
 option(ENABLE_CLANG_TIDY "Enable static analysis with clang-tidy" OFF)
 option(ENABLE_INCLUDE_WHAT_YOU_USE "Enable static analysis with include-what-you-use" OFF)
+set(ENABLE_QT_DEPRECATION_CHECK "" CACHE STRING "Define hex value (e.g. 0x061100 for Qt 6.11) for testing for deprecations. Empty = OFF")
 
 if(ENABLE_CPPCHECK)
   find_program(CPPCHECK cppcheck)
@@ -34,4 +35,9 @@ if(ENABLE_INCLUDE_WHAT_YOU_USE)
   else()
     message(SEND_ERROR "include-what-you-use requested but executable not found")
   endif()
+endif()
+
+if(NOT "${ENABLE_QT_DEPRECATION_CHECK}" STREQUAL "")
+    message(STATUS "Qt deprecations warnings enabled for: ${ENABLE_QT_DEPRECATION_CHECK}")
+    add_compile_definitions(QT_DISABLE_DEPRECATED_BEFORE=${ENABLE_QT_DEPRECATION_CHECK})
 endif()
