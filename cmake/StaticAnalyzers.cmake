5e44fadefe395e39cb4174eba5c86d9979c1a553
El Thoro <ElTh0r0@users.noreply.github.com>
2026-04-06 23:15:25 +0200

    Cmake Analyzers (#4613)
    * Add Qt deprecation check
    
    * Add Analyzer status message
    
    * Fix cppcheck

--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,18 +1,19 @@
 option(ENABLE_CPPCHECK "Enable static analysis with cppcheck" OFF)
 option(ENABLE_CLANG_TIDY "Enable static analysis with clang-tidy" OFF)
 option(ENABLE_INCLUDE_WHAT_YOU_USE "Enable static analysis with include-what-you-use" OFF)
+set(ENABLE_QT_DEPRECATION_CHECK "" CACHE STRING "Define hex value (e.g. 0x061100 for Qt 6.11) for testing for deprecations. Empty = OFF")
 
 if(ENABLE_CPPCHECK)
   find_program(CPPCHECK cppcheck)
   if(CPPCHECK)
+    message(STATUS "CPPCHECK is enabled!")
     set(CMAKE_CXX_CPPCHECK
         ${CPPCHECK}
+        --library=qt
         --suppress=missingInclude
         --enable=all
         --inline-suppr
-        --inconclusive
-        -i
-        ${CMAKE_SOURCE_DIR}/imgui/lib)
+        --inconclusive)
   else()
     message(SEND_ERROR "cppcheck requested but executable not found")
   endif()
@@ -21,6 +22,7 @@ endif()
 if(ENABLE_CLANG_TIDY)
   find_program(CLANGTIDY clang-tidy)
   if(CLANGTIDY)
+    message(STATUS "CLANGTIDY is enabled!")
     set(CMAKE_CXX_CLANG_TIDY ${CLANGTIDY} -extra-arg=-Wno-unknown-warning-option)
   else()
     message(SEND_ERROR "clang-tidy requested but executable not found")
@@ -30,8 +32,14 @@ endif()
 if(ENABLE_INCLUDE_WHAT_YOU_USE)
   find_program(INCLUDE_WHAT_YOU_USE include-what-you-use)
   if(INCLUDE_WHAT_YOU_USE)
+    message(STATUS "INCLUDE_WHAT_YOU_USE is enabled!")
     set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE ${INCLUDE_WHAT_YOU_USE})
   else()
     message(SEND_ERROR "include-what-you-use requested but executable not found")
   endif()
+endif()
+
+if(NOT "${ENABLE_QT_DEPRECATION_CHECK}" STREQUAL "")
+    message(STATUS "Qt deprecations warnings enabled for: ${ENABLE_QT_DEPRECATION_CHECK}")
+    add_compile_definitions(QT_DISABLE_DEPRECATED_BEFORE=${ENABLE_QT_DEPRECATION_CHECK})
 endif()
