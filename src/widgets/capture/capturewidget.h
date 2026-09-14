2c41f361feabc8fe0e439e29b872d9b500be76c3
Habibi Ahmad Aziz <habibiahmadaziz@gmail.com>
2026-09-14 20:14:34 +0700

    feat: add QrResultWidget forward declaration and member to CaptureWidget (Step 4.1)

--- src/widgets/capture/capturewidget.h
+++ src/widgets/capture/capturewidget.h
@@ -41,6 +41,9 @@ class UpdateNotificationWidget;
 #endif
 class UtilityPanel;
 class SidePanelWidget;
+#ifdef ENABLE_QR_DECODER
+class QrResultWidget;
+#endif
 
 class CaptureWidget : public QWidget
 {
@@ -232,4 +235,9 @@ private:
     int m_gridSize{ 10 };
 
     bool m_clipboardWorkaroundDone{ false };
+
+#ifdef ENABLE_QR_DECODER
+    QrResultWidget* m_qrResultWidget;
+    void checkForQrCode();
+#endif
 };
